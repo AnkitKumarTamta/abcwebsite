87c2d7026f08a5295069c4f61419819c8010897a
Ankit <92858106+AnkitKumarTamta@users.noreply.github.com>
2022-12-13 12:37:48 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,17 +10,17 @@ const Header = () => {
         <a className="navbar-brand text-primary" href="/">
           <span className="logo">ABC Inc</span>
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-toggle="collapse"
+//           data-target="#navbarNav"
+//           aria-controls="navbarNav"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
